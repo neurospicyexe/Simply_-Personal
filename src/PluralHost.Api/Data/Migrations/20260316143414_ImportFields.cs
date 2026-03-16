8807e6b6594de6e1191c748f03b05ba16135a822
Crash <neurospicyexe@gmail.com>
2026-03-16 09:34:33 -0500

    feat: EF migration for PkId, Birthday, SpFieldId columns

---
+++ src/PluralHost.Api/Data/Migrations/20260316143414_ImportFields.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PluralHost.Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImportFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Birthday",
+                table: "Members",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PkId",
+                table: "Members",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SpFieldId",
+                table: "CustomFields",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Birthday",
+                table: "Members");
+
+            migrationBuilder.DropColumn(
+                name: "PkId",
+                table: "Members");
+
+            migrationBuilder.DropColumn(
+                name: "SpFieldId",
+                table: "CustomFields");
+        }
+    }
+}
