34b5ff5ce6e443da79732f61a87519fef41f8616
Crash <neurospicyexe@gmail.com>
2026-03-23 10:47:13 -0500

    fix: avatar pencil -- remove circle, pink icon with drop shadow

--- src/PluralHost.Web/src/components/tabs/EssenceTab.tsx
+++ src/PluralHost.Web/src/components/tabs/EssenceTab.tsx
@@ -140,7 +140,7 @@ export default function EssenceTab({ member, groups }: Props) {
             disabled={uploading}
             type="button"
           >
-            <Pencil size={8} strokeWidth={2.5} />
+            <Pencil size={14} strokeWidth={2.5} />
           </button>
           <input
             ref={fileInputRef}
