b7fe0a9d76320b54647d165d2c31197677eda48e
Crash <neurospicyexe@gmail.com>
2026-04-03 14:41:30 -0500

    fix: add isCustom to useEffect deps in StatusPickerSheet

--- src/PluralHost.Web/src/components/StatusPickerSheet.tsx
+++ src/PluralHost.Web/src/components/StatusPickerSheet.tsx
@@ -24,7 +24,7 @@ export default function StatusPickerSheet({
 
   useEffect(() => {
     if (isOpen) setFreetext(isCustom ? currentStatus : '')
-  }, [isOpen, currentStatus])
+  }, [isOpen, currentStatus, isCustom])
 
   const handleSelect = (value: string) => {
     onSelect(value)
